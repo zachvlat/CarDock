54fcdc9592ab95c455887def6cfa8385f906464c
zatsando <zvracy13@gmail.com>
2021-10-01 15:57:44 +0300

    Initial commit

---
+++ app/src/main/java/com/zatsando/cardock/MainActivity.java
@@ -0,0 +1,40 @@
+package com.zatsando.cardock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_SELECT_CONTACT = DEFAULT_KEYS_DIALER;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openMaps(View view) {
+        Uri gmmIntentUri = Uri.parse("geo:37.983810, 23.727539");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+    }
+
+    public void openMusic(View view) {
+        Intent intent = new Intent(MediaStore.INTENT_ACTION_MUSIC_PLAYER);
+        startActivity(intent);
+    }
+
+    public void openPhone(View view) {
+
+    }
+
+    public void getSpeed(){
+
+    }
+}
